088c535943bdcc35f825f72c0eac70149fa21710
vincentzyc <vincentzyc@qq.com>
2022-02-26 17:00:28 +0800

    🎉 init: init

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 App({
-  onLaunch: function () {
-
+  onLaunch(options) {
+    if (options.query.pid) this.globalData.pid = options.query.pid
+    this.globalData.query = options.query
+  },
+  setGlobal(key, value) {
+    this.globalData[key] = value
+  },
+  getGlobal(key) {
+    return this.globalData[key] || null
+  },
+  globalData: {
+    pid: '25416',
+    query: '',
+    ipRegion: []
   }
 })
