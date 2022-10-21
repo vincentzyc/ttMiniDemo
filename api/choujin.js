1f592c0f1e265d2c19d261592b45478254393947
vincentzyc <vincentzyc@qq.com>
2022-10-21 11:06:10 +0800

    ✨ feat: 组件化init

--- api/choujin.js
+++ api/choujin.js
@@ -1,71 +1,86 @@
-export const cjBaseUrl = "https://test.cn"
+// export const cjBaseUrl = "https://card-api.liulianglf.cn"
+export const cjBaseUrl = "https://test-card.liulianglf.cn"
 
 export const Choujin = [{
-  name: "submitForm",
-  url: "/service/onlineSaleCard/handleOrder"
+    name: "submitForm",
+    url: "/service/onlineSaleCard/handleOrder"
 }, {
-  name: "getPageId",
-  url: "/service/jimPenn/page_id"
+    name: "getPageId",
+    url: "/service/jimPenn/page_id"
 }, {
-  name: "getCityInfo",
-  url: "/api/product/h5_city_info"
+    name: "getNewPageId",
+    url: "/service/jimPenn/pageIdLocation"
 }, {
-  name: "getAuthCode",
-  url: "/service/onlineSaleCard/getAuthCode"
+    name: "getCityInfo",
+    url: "/api/product/h5_city_info"
 }, {
-  name: "collectData",
-  url: "/service/onlineSaleCard/collect"
+    name: "getAuthCode",
+    url: "/service/onlineSaleCard/getAuthCode"
 }, {
-  name: "disperOrder",
-  url: "/service/onlineSaleCard/disperOrder"
+    name: "collectData",
+    url: "/service/onlineSaleCard/collect"
 }, {
-  name: "getHandleNoItem",
-  url: "/service/onlineSaleCard/getHandleNoItem",
-  getError: true,
-  getAllData: true
+    name: "disperOrder",
+    url: "/service/onlineSaleCard/disperOrder"
 }, {
-  name: "addJsInfo",
-  url: "/service/onlineSaleCard/addJsInfo"
+    name: "getHandleNoItem",
+    url: "/service/onlineSaleCard/getHandleNoItem",
+    getError: true,
+    getAllData: true
 }, {
-  name: "getIpRegion",
-  url: "/service/onlineSaleCard/getIpRegion",
-  getError: true,
-  getAllData: true
+    name: "addJsInfo",
+    url: "/service/onlineSaleCard/addJsInfo"
 }, {
-  name: "broadbandOrder",
-  url: "/api/onlineSaleBroadband/handleOrder"
+    name: "getIpRegion",
+    url: "/service/onlineSaleCard/getIpRegion",
+    getError: true,
+    getAllData: true
 }, {
-  name: "collectOrder",
-  url: "/service/onlineSaleCard/collectOrder"
+    name: "getNewIpRegion",
+    url: "https://region.cooldl.cn/service/onlineSaleCard/getIpRegion",
+    getError: true,
+    getAllData: true
 }, {
-  name: "getPrettyNoItem",
-  url: "/service/onlineSaleCard/getPrettyNoItem",
-  getError: true,
-  getAllData: true
+    name: "broadbandOrder",
+    url: "/api/onlineSaleBroadband/handleOrder"
 }, {
-  name: "getPrettyMixItem",
-  url: "/service/onlineSaleCard/getPrettyMixItem",
-  getError: true,
-  getAllData: true
+    name: "collectOrder",
+    url: "/service/onlineSaleCard/collectOrder"
 }, {
-  name: "lockNumber",
-  url: "/service/onlineSaleCard/lockNumber"
+    name: "getPrettyNoItem",
+    url: "/service/onlineSaleCard/getPrettyNoItem",
+    getError: true,
+    getAllData: true
 }, {
-  name: "getJumpPage",
-  url: "/api/jumpPage/getJumpPage"
+    name: "getPrettyMixItem",
+    url: "/service/onlineSaleCard/getPrettyMixItem",
+    getError: true,
+    getAllData: true
 }, {
-  name: "wapPrePay",
-  url: "/api/pay/wapPrePay",
-  getAllData: true
-},{
-  name:"authCode",
-  url:"/api/appOrderDetails/authCode"
-},{
-  name:"orderList",
-  url:"/api/appOrderDetails/list"
-},{
-  name:"verifyAuthCode",
-  url:"/api/appOrderDetails/verifyAuthCode"
+    name: "lockNumber",
+    url: "/service/onlineSaleCard/lockNumber"
+}, {
+    name: "getJumpPage",
+    url: "/api/jumpPage/getJumpPage"
+}, {
+    name: "wapPrePay",
+    url: "/api/pay/wapPrePay",
+    getAllData: true
+}, {
+    name: "getProtocol",
+    url: "/api/operator/getProtocol",
+    type: "get",
+    getAllData: true
+}, {
+    name: "authCode",
+    url: "/api/appOrderDetails/authCode"
+}, {
+    name: "orderList",
+    url: "/api/appOrderDetails/list"
+}, {
+    name: "verifyAuthCode",
+    url: "/api/appOrderDetails/verifyAuthCode"
 }]
+
 
 export default Choujin
