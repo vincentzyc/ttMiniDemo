db6837056ee1a61fe649a5e65dd4ffe5805b91ef
vincentzyc <vincentzyc@qq.com>
2022-10-23 01:03:01 +0800

    ✨ feat: 组件化init

--- pages/h5/widgets/WgImage/WgImage.js
+++ pages/h5/widgets/WgImage/WgImage.js
@@ -3,6 +3,7 @@ import { objStyle2Str, isLink } from '../../../../utils/index'
 const app = getApp();
 
 Component({
+  cpPopup: null,
   data: {
     wgStyle: ''
   },
@@ -17,12 +18,17 @@ Component({
     },
   },
   methods: {
+    refPopup(ref) {
+      // 存储自定义组件实例，方便以后调用
+      this.cpPopup = ref;
+    },
     goUrl(e) {
-      console.log(e)
-      app.busImage.emit.testEmit('HAHA')
-      setTimeout(() => {
-        app.busImage.emit.testEmit('hehe')
-      }, 1100);
+      console.log(e, this.data.wgItem)
+      this.cpPopup.openPopup()
+      // app.busImage.emit.testEmit('HAHA')
+      // setTimeout(() => {
+      //   app.busImage.emit.testEmit('hehe')
+      // }, 1100);
       // const link = e.currentTarget.dataset.otherlink
       // if (isLink(link)) {
       //   tt.navigateTo({
