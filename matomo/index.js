64ad0dc363ba18399398da9e5c6e05eb15cddf4c
vincentzyc <vincentzyc@qq.com>
2022-12-02 18:34:38 +0800

    🎉 init: init

--- matomo/index.js
+++ matomo/index.js
@@ -1,23 +1,98 @@
 
-class Matomo {
-  constructor(miniType) {
-    this.miniType = miniType || tt // 小程序类型
-    this.configTrackerUrl = 'https://dw-m.jetmobo.com/tj'
+// class Tracker {
+class Tracker {
+  constructor(miniType, siteId) {
+    this.miniType = miniType // 小程序类型
+    this.configTrackerSiteId = siteId
+    this.configTrackerUrl = '//dw-m.jetmobo.com/tj'
     this.configRequestMethod = 'GET'
     this.configRequestContentType = 'application/x-www-form-urlencoded; charset=UTF-8'
-    this.browserFeatures = {}
+    this.uvidStorageKey = 'MatomoUvid'
+    this.uvid = ''
+    this.pvid = ''
   }
   /**
-   * 初始化一个跟踪器
-   * @param {String} siteId
-   */
-  initTracker(siteId) {
+  * 初始化一个跟踪器
+  * @param {String} siteId
+  */
+  initTracker(obj) {
+    // obj.siteId
+  }
+  trackPageView() {
 
+  }
+  trackEvent(category, action, name, value) {
+    // Category and Action are required parameters
+    if (!isNumberOrHasLength(category) || !isNumberOrHasLength(action)) {
+      logConsoleError('Error while logging event: Parameters `category` and `action` must not be empty or filled with whitespaces');
+      return false;
+    }
+    var request = getRequest(buildEventRequest(category, action, name, value));
+    sendRequest(request);
+  }
+  buildEventRequest(category, action, name, value) {
+    return 'e_c=' + encodeURIComponent(category)
+      + '&e_a=' + encodeURIComponent(action)
+      + (isDefined(name) ? '&e_n=' + encodeURIComponent(name) : '')
+      + (isDefined(value) ? '&e_v=' + encodeURIComponent(value) : '')
+      + '&ca=1';
+  }
+  getRequest(request) {
+    const now = new Date()
+    request += '&idsite=' + configTrackerSiteId +
+      '&rec=1' +
+      '&r=' + String(Math.random()).slice(2, 8) + // keep the string to a minimum
+      '&h=' + now.getHours() + '&m=' + now.getMinutes() + '&s=' + now.getSeconds() +
+      '&url=' + getUrl(currentUrl) +
+      (configReferrerUrl.length ? '&urlref=' + encodeURIComponent(purify(configReferrerUrl)) : '') +
+      (isNumberOrHasLength(configUserId) ? '&uid=' + encodeURIComponent(configUserId) : '') +
+      '&_id=' + cookieVisitorIdValues.uuid +
+
+      '&_idn=' + cookieVisitorIdValues.newVisitor + // currently unused
+      (campaignNameDetected.length ? '&_rcn=' + encodeURIComponent(campaignNameDetected) : '') +
+      (campaignKeywordDetected.length ? '&_rck=' + encodeURIComponent(campaignKeywordDetected) : '') +
+      '&_refts=' + referralTs +
+      (String(referralUrl).length ? '&_ref=' + encodeURIComponent(purify(referralUrl.slice(0, referralUrlMaxLength))) : '') +
+      (charSet ? '&cs=' + encodeURIComponent(charSet) : '') +
+      '&send_image=0';
+    return request
+  }
+  // 获取url，把参数拼接
+  getUrl() {
+
+  }
+  initVisitorId() {
+    if (this.uvid) return this.uvid
+    try {
+      var storageUvid = this.miniType.getStorageSync(this.uvidStorageKey);
+      if (storageUvid) {
+        this.uvid = storageUvid
+        return storageUvid
+      }
+    } catch (error) {
+      logConsoleError('getStorageSync调用失败');
+    }
+    var randomUuid = this.generateRandomUuid()
+    this.uvid = randomUuid
+    this.setVisitorIdStorage(randomUuid)
+    return randomUuid
+  }
+  setVisitorIdStorage(uuid) {
+    try {
+      this.miniType.setStorageSync(this.uvidStorageKey, uuid);
+    } catch (error) {
+      logConsoleError('setStorageSync调用失败');
+    }
   }
   // 生成 uvid
   generateRandomUuid() {
-    var browserFeatures = this.miniType.getSystemInfoSync()
-    return hash(
+    var browserFeatures = {}
+    try {
+      browserFeatures = this.miniType.getSystemInfoSync(true)
+    } catch (error) {
+      logConsoleError("获取系统信息失败: ", error);
+    }
+    return sha1(
       JSON.stringify(browserFeatures) +
       (new Date()).getTime() +
       Math.random()
@@ -48,16 +123,123 @@ class Matomo {
             callback && callback()
           },
           fail(res) {
-            console.log('request fail', miniType.request)
+            logConsoleError('request fail', miniType.request)
           }
         })
       } catch (error) {
-        console.log('request catch error', error)
+        logConsoleError('request catch error', error)
       }
     }, 50)
   }
 }
 
+export default function matomo(miniType, siteId) {
+  if (isObjectEmpty(miniType) || !isNumberOrHasLength(siteId)) {
+    logConsoleError('初始化matomo时，小程序类型 "miniType" 、站点id "siteId" 不能为空');
+    return false;
+  }
+  var tracker = new Tracker(miniType, siteId);
+  tracker.initVisitorId()
+  // asyncTrackers.push(tracker);
+
+  // _paq = applyMethodsInOrder(_paq, applyFirst);
+
+  // // apply the queue of actions
+  // for (iterator = 0; iterator < _paq.length; iterator++) {
+  //   if (_paq[iterator]) {
+  //     apply(_paq[iterator]);
+  //   }
+  // }
+
+  // // replace initialization array with proxy object
+  // _paq = new TrackerProxy();
+
+  // Matomo.trigger('TrackerAdded', [tracker]);
+  miniType.matomo = tracker
+  return tracker;
+}
+
+/*
+ * Removes hash tag from the URL
+ *
+ * URLs are purified before being recorded in the cookie,
+ * or before being sent as GET parameters
+ */
+function purify(url) {
+  var targetPattern;
+  // we need to remove this parameter here, they wouldn't be removed in Matomo tracker otherwise eg
+  // for outlinks or referrers
+  url = removeUrlParameter(url, configVisitorIdUrlParameter);
+  if (configDiscardHashTag) {
+    targetPattern = new RegExp('#.*');
+    return url.replace(targetPattern, '');
+  }
+  return url;
+}
+
+/**
+ * Logs an error in the console.
+ *  Note: it does not generate a JavaScript error, so make sure to also generate an error if needed.
+ * @param message
+ */
+function logConsoleError(message) {
+  // needed to write it this way for jslint
+  var consoleType = typeof console;
+  if (consoleType !== 'undefined' && console && console.error) {
+    console.error(message);
+  }
+}
+/*
+ * Is property defined?
+ */
+function isDefined(property) {
+  var propertyType = typeof property;
+
+  return propertyType !== 'undefined';
+}
+
+/*
+ * Is property a function?
+ */
+function isFunction(property) {
+  return typeof property === 'function';
+}
+
+/*
+ * Is property an object?
+ * @return bool Returns true if property is null, an Object, or subclass of Object (i.e., an instanceof String, Date, etc.)
+ */
+function isObject(property) {
+  return typeof property === 'object';
+}
+
+function isString(property) {
+  return typeof property === 'string' || property instanceof String;
+}
+
+function isNumber(property) {
+  return typeof property === 'number' || property instanceof Number;
+}
+
+function isNumberOrHasLength(property) {
+  return isDefined(property) && (isNumber(property) || (isString(property) && property.length));
+}
+
+function isObjectEmpty(property) {
+  if (!property) {
+    return true;
+  }
+
+  var i;
+  var isEmpty = true;
+  for (i in property) {
+    if (Object.prototype.hasOwnProperty.call(property, i)) {
+      isEmpty = false;
+    }
+  }
+
+  return isEmpty;
+}
 function utf8_encode(argString) {
   return decodeURIComponent(encodeURIComponent(argString))
 }
